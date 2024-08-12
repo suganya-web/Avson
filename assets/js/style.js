d822219784edd497173330b21687313d625ff78d
suganya-web <suganya.donzz508@gmail.com>
2024-08-12 10:43:02 +0530

    Add files via upload

---
+++ assets/js/style.js
@@ -0,0 +1,21 @@
+// window.onscroll = function() {stickyHeader()};
+
+// var header = document.getElementById("header");
+// var sticky = header.offsetTop;
+// var minWidth = 798;
+
+// function stickyHeader() {
+//     if (window.innerWidth >= minWidth) {
+//         if (window.pageYOffset > sticky) {
+//             header.classList.add("sticky-header");
+//         } else {
+//             header.classList.remove("sticky-header");
+//         }
+//     } else {
+//         header.classList.remove("sticky-header");
+//     }
+// }
+
+// window.addEventListener('resize', stickyHeader);
+
+// window.addEventListener('load', stickyHeader);
